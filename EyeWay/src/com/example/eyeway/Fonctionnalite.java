ee0a710a5f434da9e01762b470496a5ed23d60c0
Christophebordier28@gmail.com <Christophebordier28@gmail.com@9f2d11f9-20c1-6558-df21-47adeac02fb6>
2012-12-07 18:21:45 +0000

    layout : menu, listview avec border, titlebar. Todo : gestion des clicks sur les items

---
+++ EyeWay/src/com/example/eyeway/Fonctionnalite.java
@@ -0,0 +1,17 @@
+package com.example.eyeway;
+
+public class Fonctionnalite {
+	public int icon;
+	public String title;
+	
+	public Fonctionnalite(){
+		super();
+	}
+
+	public Fonctionnalite(int icon, String title) {
+		super();
+		this.icon = icon;
+		this.title = title;
+	}
+
+}
