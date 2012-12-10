fff8b57a3b6715a766579c08df7160e9c574339c
B.Boyer28@gmail.com <B.Boyer28@gmail.com@9f2d11f9-20c1-6558-df21-47adeac02fb6>
2012-12-10 14:00:26 +0000

    Ajout getteurs sur les classes Lieu et Photo.

--- trunk/EyeWay/src/com/example/eyeway/fouilleDedonne/Lieu.java
+++ trunk/EyeWay/src/com/example/eyeway/fouilleDedonne/Lieu.java
@@ -5,14 +5,15 @@ import java.util.List;
 // PAS ENCORE TEST
 public class Lieu {
 	
-	double latitude, longitude;
-	String icone;
-	String nom;
-	String reference;
-	String vicinity;
-	List<Photo> photos;
-	List<String> types;
+	private double latitude, longitude;
+	private String icone;
+	private String nom;
+	private String reference;
+	private String vicinity;
+	private List<Photo> photos;
+	private List<String> types;
 	
+
 	public Lieu(double lat, double lng, String icon, String id, String name,
 				List<Photo> pho, String ref, List<String> typ, String vic) {
 		
@@ -24,6 +25,61 @@ public class Lieu {
 		vicinity=vic;
 		photos=pho;
 		types=typ;
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public String getIcone() {
+		return icone;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public String getVicinity() {
+		return vicinity;
+	}
+
+	public List<Photo> getPhotos() {
+		return photos;
+	}
+
+	public List<String> getTypes() {
+		return types;
+	}
+	
+	public String toString() {
+		String toString="";
+		toString+="-------------------\n"
+				+"("+latitude+","+longitude+")\n"
+				+"icone : "+icone+")\n"
+				+"nom : "+nom+")\n"
+				+"reference : "+reference+")\n"
+				+"vicinity : "+vicinity+")\n"
+				+"types : ";
+		
+		for(String type : types) {
+			toString+=type+" ";
+		}
+		
+		toString+="\n\n";
+		for(Photo p : photos) {
+			toString+=" h : "+p.getHeight()+" w : "+p.getWidth()+
+					"\nphoto ref : "+p.getPhoto_reference()+"\n\n";
+		}
+
+		return toString;
 	}
 	
 }
